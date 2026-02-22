be32be5e4659a6603414058c71d86b4311e5afcf
Alazen <you@gmail.com>
2026-02-22 19:01:27 -0300

    fix: hide console window on Windows release builds Corrected the `windows_subsystem` attribute in [src-tauri/src/main.rs] by altering `#[cfg_attr(...)]` to `#![cfg_attr(...)]`. This ensures that the attribute correctly applies to the entire crate instead of just the next item, successfully preventing the `cmd` window from launching alongside the main application window.

--- perfect-world-launcher/src-tauri/src/main.rs
+++ perfect-world-launcher/src-tauri/src/main.rs
@@ -4,7 +4,7 @@ pub mod state;
 pub mod launcher;
 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
     perfect_world_launcher_lib::run()
